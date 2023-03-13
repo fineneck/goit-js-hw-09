a9d6bd2cd54170617ad37324c8c2de1a2ea28f97
OleksiiMikhieiev <babybong0993@gmail.com>
2023-03-13 02:13:17 +0200

    done

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,28 @@
+const startBtn =  document.querySelector('[data-start]');
+const stopBtn = document.querySelector('[data-stop]');
 
+let intervalId = null;
+const COLOR_DELAY = 1000;
+
+startBtn.addEventListener('click', startBtnClick);
+stopBtn.addEventListener('click', stopBtnClick);
+
+function startBtnClick() {
+  intervalId = setInterval(() => {
+    document.body.style.backgroundColor = getRandomHexColor();
+  }, COLOR_DELAY);
+  
+  startBtn.disabled = true;
+  stopBtn.disabled = false;
+};
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+};
+
+function stopBtnClick() {  
+  clearInterval(intervalId);
+
+  startBtn.disabled = false;
+  stopBtn.disabled = true;
+};
