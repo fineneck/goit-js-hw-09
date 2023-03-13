52c6e7e92bf5b9fbc4ee12431340fd4559bda68a
OleksiiMikhieiev <babybong0993@gmail.com>
2023-03-14 01:51:24 +0200

    add-refs

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -2,3 +2,18 @@ import flatpickr from "flatpickr";
 import "flatpickr/dist/flatpickr.min.css";
 import Notiflix from 'notiflix';
 // import "notiflix/dist/notiflix-3.2.6.min.css";
+
+const refs = {
+  inputEl: document.querySelector('#datetime-picker'),
+  startBtn: document.querySelector('[data-start]'),
+  timerEl: document.querySelector('.timer'),
+  fieldEl: document.querySelector('.field'),
+  daysEl: document.querySelector('[data-days]'),
+  hoursEl: document.querySelector('[data-hours]'),
+  minsEl: document.querySelector('[data-minutes]'),
+  secsEl: document.querySelector('[data-seconds]'),
+};
+
+
+
+console.log(refs);
