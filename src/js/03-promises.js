0e5271b8fc9adbda69aaaf0d8677c4eefa705d6b
OleksiiMikhieiev <babybong0993@gmail.com>
2023-03-14 23:35:32 +0200

    add-notiflix

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,3 +1,6 @@
+import Notiflix from 'notiflix';
+import "notiflix/dist/notiflix-3.2.6.min.css";
+
 function createPromise(position, delay) {
   const shouldResolve = Math.random() > 0.3;
   if (shouldResolve) {
@@ -6,3 +9,14 @@ function createPromise(position, delay) {
     // Reject
   }
 }
+
+createPromise(2, 1500)
+  .then(({ position, delay }) => {
+    Notiflix.Notify.success('✅ Fulfilled promise ${position} in ${delay}ms');
+  })
+  .catch(({ position, delay }) => {
+    Notiflix.Notify.failure('❌ Rejected promise ${position} in ${delay}ms');
+  });
+
+
+
